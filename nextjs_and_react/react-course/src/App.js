75ca7afd03c3474421ff9b96386e7293cca32a5e
Timothy Karani <kartimothy@gmail.com>
2022-07-30 19:11:44 +0300

    Added pages

--- nextjs_and_react/react-course/src/App.js
+++ nextjs_and_react/react-course/src/App.js
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+
 function App() {
   return (
-    <div>
-      <h1>Mwahaha</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<AllMeetupsPage />} />
+      <Route path="/new-meetup" element={<NewMeetupPage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+    </Routes>
   );
 }
 
---
+++ nextjs_and_react/react-course/src/pages/Favorites.js
@@ -0,0 +1,5 @@
+function FavoritesPage() {
+  return <div>My Favorites</div>;
+}
+
+export default FavoritesPage;
