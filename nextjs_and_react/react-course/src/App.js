fe8962e3e3cf321b1550ff6751b0868bfe8dfb0a
Timothy Karani <kartimothy@gmail.com>
2022-07-30 19:24:40 +0300

    Added module css

--- nextjs_and_react/react-course/src/App.js
+++ nextjs_and_react/react-course/src/App.js
@@ -2,14 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/MainNavigation";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AllMeetupsPage />} />
-      <Route path="/new-meetup" element={<NewMeetupPage />} />
-      <Route path="/favorites" element={<FavoritesPage />} />
-    </Routes>
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
   );
 }
 
