5e0c5e7571ef0078b184d55647ca77e7aff17eed
Timothy Karani <kartimothy@gmail.com>
2022-07-30 18:47:42 +0300

    First of many

--- nextjs_and_react/react-course/src/App.js
+++ nextjs_and_react/react-course/src/App.js
@@ -1,12 +1,7 @@
-import Todo from "./components/Todo";
-
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Learn NextJS on top of this" />
-      <Todo text="Add React Router or Redux state management" />
+      <h1>Mwahaha</h1>
     </div>
   );
 }
