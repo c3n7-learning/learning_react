8355491b297ca6a5751a9a23cebfd14da340badb
Timothy Karani <kartimothy@gmail.com>
2022-07-30 18:25:30 +0300

    Added modal

--- nextjs_and_react/react-course/src/App.js
+++ nextjs_and_react/react-course/src/App.js
@@ -1,13 +1,16 @@
+import Backdrop from "./components/Backdrop";
+import Modal from "./components/Modal";
+import Todo from "./components/Todo";
+
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div className="actions">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <Todo text="Learn React" />
+      <Todo text="Learn NextJS on top of this" />
+      <Todo text="Add React Router or Redux state management" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
