2ff555c4e2d83d0947739e092cd4b1c89650592d
Timothy Karani <kartimothy@gmail.com>
2022-08-10 01:10:18 +0300

    Added swr

--- nextjs_and_react/4-static-pre-rendering/pages/last-sales.js
+++ nextjs_and_react/4-static-pre-rendering/pages/last-sales.js
@@ -1,10 +1,34 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 function LastSalesPage() {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    "https://c3n7-games.firebaseio.com/sales.json",
+    fetcher
+  );
 
   useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  /*useEffect(() => {
     setIsLoading(true);
     fetch("https://c3n7-games.firebaseio.com/sales.json")
       .then((response) => response.json())
@@ -22,14 +46,18 @@ function LastSalesPage() {
         setSales(transformedSales);
         setIsLoading(false);
       });
-  }, []);
+  }, []);*/
 
-  if (isLoading) {
+  if (error) {
+    return <p>Failed to load</p>;
+  }
+
+  if (!data) {
     return <p>Loading...</p>;
   }
 
   if (!sales) {
-    return <p>No Data</p>;
+    return <p>No Sales...</p>;
   }
 
   return (
