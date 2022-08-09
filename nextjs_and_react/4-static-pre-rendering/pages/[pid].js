b959a8b69c83bdc7669c1a8e44d5604a91d0e264
Timothy Karani <kartimothy@gmail.com>
2022-08-10 00:08:00 +0300

    Prerendering 404

--- nextjs_and_react/4-static-pre-rendering/pages/[pid].js
+++ nextjs_and_react/4-static-pre-rendering/pages/[pid].js
@@ -5,9 +5,9 @@ import { Fragment } from "react";
 function ProductDetailPage(props) {
   const { loadedProduct } = props;
 
-  // if (!loadedProduct) {
-  //   return <p>Loading...</p>;
-  // }
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <Fragment>
@@ -22,11 +22,13 @@ export async function getStaticProps(context) {
 
   const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
+
+  if (!product) {
+    return { notFound: true };
+  }
 
   return {
     props: {
@@ -35,10 +37,23 @@ export async function getStaticProps(context) {
   };
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [{ params: { pid: "p1" } }],
-    fallback: "blocking",
+    paths: pathsWithParams,
+    fallback: true,
   };
 }
 
