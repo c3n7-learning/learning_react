f57351420b68872da48774990dacec788660f554
Timothy Karani <kartimothy@gmail.com>
2022-08-10 17:53:43 +0300

    Event id working

--- nextjs_and_react/6-pre-rendering-project/pages/events/[eventId].js
+++ nextjs_and_react/6-pre-rendering-project/pages/events/[eventId].js
@@ -1,17 +1,14 @@
 import { useRouter } from "next/router";
 import { Fragment } from "react";
-import { getEventById } from "../../dummy-data";
+import { getEventById, getAllEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import Button from "../../components/ui/button";
 import ErrorAlert from "../../components/error-alert/error-alert";
 
-function EventDetailPage() {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+function EventDetailPage(props) {
+  const { event } = props;
 
   if (!event) {
     return (
@@ -38,6 +35,29 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      event: event,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
 }
 
 export default EventDetailPage;
