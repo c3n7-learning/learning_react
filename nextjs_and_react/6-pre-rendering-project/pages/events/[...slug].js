edf90eb9f86c2e4bcdc4aa9550b9bca9ca9de257
Timothy Karani <kartimothy@gmail.com>
2022-08-10 17:04:17 +0300

    First of many

---
+++ nextjs_and_react/6-pre-rendering-project/pages/events/[...slug].js
@@ -0,0 +1,67 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/results-title/results-title";
+import { Fragment } from "react";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/error-alert/error-alert";
+
+function FilteredEventsPage() {
+  const router = useRouter();
+  const filteredData = router.query.slug;
+
+  if (!filteredData) {
+    return <p className="center">Loading</p>;
+  }
+
+  const filteredYear = filteredData[0];
+  const filteredMonth = filteredData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth > 12 ||
+    numMonth < 1
+  ) {
+    return (
+      <Fragment>
+        <ErrorAlert>Invalid filter. Please adjust your values</ErrorAlert>
+        <div className="center">
+          <Button linke="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length == 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>No events found for the chosen flter!</ErrorAlert>
+        <div className="center">
+          <Button linke="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
+  );
+}
+
+export default FilteredEventsPage;
---
+++ nextjs_and_react/6-pre-rendering-project/pages/events/[eventId].js
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/error-alert/error-alert";
+
+function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return (
+      <Fragment>
+        <ErrorAlert>No event found!</ErrorAlert>
+        <div className="center">
+          <Button linke="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
+}
+
+export default EventDetailPage;
