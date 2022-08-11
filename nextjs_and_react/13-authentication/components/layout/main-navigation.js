635ce0f14153351117fc879b461526785b13b896
Timothy Karani <kartimothy@gmail.com>
2022-08-11 15:31:04 +0300

    Can log out

--- nextjs_and_react/13-authentication/components/layout/main-navigation.js
+++ nextjs_and_react/13-authentication/components/layout/main-navigation.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { useSession } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 
 import classes from "./main-navigation.module.css";
 
@@ -7,6 +7,10 @@ function MainNavigation() {
   const { data: session, status } = useSession();
 
   console.log(status, session);
+
+  function logoutHandler() {
+    signOut();
+  }
 
   return (
     <header className={classes.header}>
@@ -17,7 +21,7 @@ function MainNavigation() {
       </Link>
       <nav>
         <ul>
-          {!session && !loading && (
+          {!session && status !== "loading" && (
             <li>
               <Link href="/auth">Login</Link>
             </li>
@@ -29,7 +33,7 @@ function MainNavigation() {
           )}
           {session && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
