3af409089b4b5fde1bbefaa6a9315383bf8d9d09
Timothy Karani <kartimothy@gmail.com>
2022-08-11 16:00:40 +0300

    Added server side props

--- nextjs_and_react/13-authentication/components/profile/user-profile.js
+++ nextjs_and_react/13-authentication/components/profile/user-profile.js
@@ -5,24 +5,24 @@ import classes from "./user-profile.module.css";
 
 function UserProfile() {
   // Redirect away if NOT auth
-  const [isLoading, setIsLoading] = useState(true);
-  const [loadedSession, setLoadedSession] = useState();
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [loadedSession, setLoadedSession] = useState();
 
-  const { data: session, status } = useSession();
+  // const { data: session, status } = useSession();
 
-  useEffect(() => {
-    getSession().then((session) => {
-      setLoadedSession(session);
-      setIsLoading(false);
-      if (!session) {
-        window.location.href = "/auth";
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   getSession().then((session) => {
+  //     setLoadedSession(session);
+  //     setIsLoading(false);
+  //     if (!session) {
+  //       window.location.href = "/auth";
+  //     }
+  //   });
+  // }, []);
 
-  if (isLoading) {
-    return <p className={classes.profile}>Loading...</p>;
-  }
+  // if (isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>;
+  // }
 
   return (
     <section className={classes.profile}>
