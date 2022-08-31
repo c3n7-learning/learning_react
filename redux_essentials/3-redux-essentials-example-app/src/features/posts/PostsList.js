c84d03aaf0eedbe7f949ce9d9d912b40138f413f
Timothy Karani <kartimothy@gmail.com>
2022-09-01 01:21:06 +0300

    Can update state

--- redux_essentials/3-redux-essentials-example-app/src/features/posts/PostsList.js
+++ redux_essentials/3-redux-essentials-example-app/src/features/posts/PostsList.js
@@ -4,7 +4,7 @@ export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
 
   const renderedPosts = posts.map((post) => (
-    <article class="post-excerpt" key={post.id}>
+    <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
     </article>
