176a42ff76ecb362760665b0b7a45590329c851e
Timothy Karani <kartimothy@gmail.com>
2022-09-01 16:35:04 +0300

    Added time stamps

--- redux_essentials/3-redux-essentials-example-app/src/features/posts/postsSlice.js
+++ redux_essentials/3-redux-essentials-example-app/src/features/posts/postsSlice.js
@@ -1,8 +1,21 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [
-  { id: '1', title: 'First Post!', content: 'Hello' },
-  { id: '2', title: 'Second Post', content: 'More text' },
+  {
+    id: '1',
+    title: 'First Post!',
+    content: 'Hello',
+    user: '2',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+  },
+  {
+    id: '2',
+    title: 'Second Post',
+    content: 'More text',
+    user: '1',
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+  },
 ]
 
 const postsSlice = createSlice({
@@ -17,6 +30,7 @@ const postsSlice = createSlice({
         return {
           payload: {
             id: nanoid(),
+            date: new Date().toISOString(),
             title,
             content,
             user: userId,
