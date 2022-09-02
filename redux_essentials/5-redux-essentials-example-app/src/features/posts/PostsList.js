d2e00560b9542bf2d388dcc91d7bb20c8fc6d5e0
Timothy Karani <kartimothy@gmail.com>
2022-09-02 11:10:39 +0300

    Can load posts

--- redux_essentials/5-redux-essentials-example-app/src/features/posts/PostsList.js
+++ redux_essentials/5-redux-essentials-example-app/src/features/posts/PostsList.js
@@ -1,12 +1,22 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
 import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
-import { selectAllPosts } from './postsSlice'
+import { fetchPosts, selectAllPosts } from './postsSlice'
+import { useEffect } from 'react'
 
 export const PostsList = () => {
+  const dispatch = useDispatch()
   const posts = useSelector(selectAllPosts)
+
+  const postStatus = useSelector((state) => state.posts.status)
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchPosts())
+    }
+  }, [postStatus, dispatch])
+
   const orderedPosts = posts
     .slice()
     .sort((a, b) => b.date.localeCompare(a.date))
